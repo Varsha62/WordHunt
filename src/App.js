080e425f4480a5b70a0ff95ce1c7bd69302682ba
Varsha62 <vinsisahu3003@gmail.com>
2023-02-23 12:12:12 +0530

    my first website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+// import { purple } from '@mui/material/colors';
+import axios from 'axios';
+import { Container } from '@mui/system';
+// import {Switch,withStyles} from '@mui/material';
+import Header from './components/Header/Header';
+import Definations from './components/Definations/Definations';
+
+
 
 function App() {
+
+  const [word, setWord] = useState("");
+  const [meanings, setMeanings] = useState([]);
+  const [category , setCategory]=useState("en");
+
+  // const PurpleSwitch = withStyles({
+  //   switchBase: {
+  //     color: purple[50],
+  //     "&$checked": {
+  //       color: purple[900],
+  //     },
+  //     "&$checked + $track": {
+  //       backgroundColor: purple[500],
+  //     },
+  //   },
+  //   checked: {},
+  //   track: {},
+  // })(Switch);
+
+  const dictionaryApi = async() =>{
+    try{
+      const data = await axios.get(
+        `https://api.dictionaryapi.dev/api/v2/entries/${category}/${word}`
+      );
+      setMeanings(data.data);
+    }catch(error) {
+      console.log(error);
+    }
+  };
+  console.log(meanings);
+  useEffect(() =>{
+    dictionaryApi();
+
+  },[word,category] );
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"
+     style={{height:'100vh',
+     backgroundColor:'#2B2c34',color:'white'}}
+    >
+    <Container
+     maxWidth = "md"
+     style={{display:'flex',
+     flexDirection:'column',
+     height:'100vh',}}>
+   <Header 
+   category ={category} 
+   setCategory={setCategory}
+    word={word} 
+    setWord={setWord}
+    />
+    {meanings && (
+      <Definations word ={word} meanings={meanings} category={category}/>
+    )}
+ 
+    </Container>
     </div>
   );
 }
+// import { grey } from '@mui/material/colors';
 
 export default App;
